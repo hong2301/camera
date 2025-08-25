96fdcceb208737dbe434f1984b47e2d98af481ee
hong <honononong@icloud.com>
2025-08-25 17:56:18 +0800

    refactor(): 结构改变

--- server/src/api/electrical_machinery/index.js
+++ server/src/api/electrical_machinery/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
-const electricalMachineryHandler = require('../router_handler/electrical_machinery')
+const electricalMachineryHandler = require('./handler')
+const { reg_btn_schema } = require('./schema')
 const expressJoi = require('@escook/express-joi')
-const { reg_btn_schema } = require('../schema/electrical_machinery')
 
 router.post('/btn', expressJoi(reg_btn_schema), electricalMachineryHandler.btn)
 
--- server/src/app.js
+++ server/src/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 // 路由
 const testRouter = require('./router/test')
 app.use('/api', testRouter)
-const electricalMachineryRouter = require('./router/electrical_machinery')
+const electricalMachineryRouter = require('./api/electrical_machinery/index')
 app.use('/electrical_machinery', electricalMachineryRouter)
 
 // 错误处理中间件
